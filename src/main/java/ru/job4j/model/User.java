fffefc3c9f441ffa88ff98f28ec58a537fa14c65
Lyubov <n.lubovj@gmail.com>
2024-01-03 00:37:00 +0300

    Add User class, add authorization and registration pages

---
+++ src/main/java/ru/job4j/model/User.java
@@ -0,0 +1,27 @@
+package ru.job4j.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+}
